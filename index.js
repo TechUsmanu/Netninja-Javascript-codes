130b929a92781815a1bfcaa72e76c9d3a7a7e477
Usman Umar Garba <110467626+techusman-codes@users.noreply.github.com>
2024-02-08 13:41:14 -0800

    Add files via upload
    some part of document object model codes the docmet object model codes

--- index.js
+++ index.js
@@ -20,16 +20,52 @@
 
 // to grab multiple element use querySelectorAll
 
-const paras = document.querySelectorAll('p')
-const error = document.querySelectorAll('.error');
-console.log(error)
-console.log(paras[2]);
+// const paras = document.querySelectorAll('p')
+// const error = document.querySelectorAll('.error');
+// console.log(error)
+// console.log(paras[2]);
 
-// get element by ID
-  const title = document.getElementById('page-title');
-  console.log(title);
+// // get element by ID
+//   const title = document.getElementById('page-title');
+//   console.log(title);
 
-// grab element by their class name
- const errors = documet.getElementsByclassName('error');
- console.log(errors);
-// grab element ny their tag name
+// to change a text or element inside a HTML tag
+
+const para = document.querySelector('p');
+
+// console.log(para.innerText);
+// to change something on the page
+// para.innerText = 'ninjas are awesome!';
+
+// to change the text of several items
+
+const paras = document.querySelectorAll('p');
+
+// paras.forEach(para => {
+//   console.log(para.innerText);
+//   // to udte the concept
+//   para.innerText += 'new text';
+// }
+  
+// );
+// to chnage the HTML on somthing
+
+const content = document.querySelector('.content');
+
+// console.log(content.innerHTML);
+
+// to update  with another thing
+// += to append and = normal equal to overide it
+
+
+// content.innerHTML += '<h2>THIS IS A NEW H2</h2>';
+
+const people = ['mario', 'luigi', 'yoshi'];
+
+people.forEach(person => {
+    content.innerHTML += `<p>${person}</p>`;
+});
+
+
+
+
